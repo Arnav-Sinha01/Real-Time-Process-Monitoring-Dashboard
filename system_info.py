53e97a8e01b2b2dea8e4d92ca7c1a9fc36bf102e
Shreyash Jain <2005shreyashjain@gmail.com>
2025-03-27 22:54:50 +0530

    Added the process list view

--- system_info.py
+++ system_info.py
@@ -1,18 +1,21 @@
 import psutil
 
+
 def get_system_stats():
     cpu_usage = round(psutil.cpu_percent(), 2)
     memory_usage = round(psutil.virtual_memory().percent, 2)
     disk_usage = round(psutil.disk_usage('/').percent, 2)
     return cpu_usage, memory_usage, disk_usage
 
+
 def get_process_list(search_text=""):
-    processes = [(p.info["pid"], p.info["name"], round(p.info["cpu_percent"], 2), round(p.info["memory_percent"], 2)) 
+    processes = [(p.info["pid"], p.info["name"], round(p.info["cpu_percent"], 2), round(p.info["memory_percent"], 2))
                  for p in psutil.process_iter(["pid", "name", "cpu_percent", "memory_percent"])]
     processes.sort(key=lambda x: x[2], reverse=True)
     if search_text:
         processes = [p for p in processes if search_text in p[1].lower()]
     return processes
+
 
 def terminate_process(pid):
     try:
